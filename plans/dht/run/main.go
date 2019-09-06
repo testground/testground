064c544b387d041bacfbb01f4f935c453b1d1da8
Raúl Kripalani <raul@protocol.ai>
2019-09-06 19:11:18 +0100

    make dht test synchronize via redis.

--- plans/dht/run/main.go
+++ plans/dht/run/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/google/uuid"
+
 	"github.com/ipfs/testground/plans/dht"
 	"github.com/logrusorgru/aurora"
 )
@@ -15,21 +17,23 @@ func main() {
 	_ = os.Setenv("TEST_BRANCH", "master")
 	_ = os.Setenv("TEST_CASE", "lookup_peers")
 	_ = os.Setenv("TEST_TAG", "")
-	_ = os.Setenv("TEST_RUN", "1")
+	_ = os.Setenv("TEST_RUN", uuid.New().String())
 
 	var tcs []*dht.LookupPeersTC
 	for i := 0; i < 100; i++ {
 		fmt.Println(aurora.Sprintf(aurora.Blue(aurora.Bold("starting instance: %d")), i))
-		tc := &dht.LookupPeersTC{Instance: i}
+		tc := &dht.LookupPeersTC{Instance: i, Count: 100}
 		tcs = append(tcs, tc)
 		go tc.Execute()
 	}
 
-	for range time.Tick(5 * time.Second) {
-		for _, tc := range tcs {
-			fmt.Println(aurora.Sprintf(aurora.Yellow(aurora.Italic("instance %d: received %d events")), tc.Instance, tc.EventsReceived))
-		}
-		fmt.Println("-----------")
-	}
+	time.Sleep(1 * time.Hour)
+
+	// for range time.Tick(5 * time.Second) {
+	// 	for _, tc := range tcs {
+	// 		fmt.Println(aurora.Sprintf(aurora.Yellow(aurora.Italic("instance %d: received %d events")), tc.Instance, tc.EventsReceived))
+	// 	}
+	// 	fmt.Println("-----------")
+	// }
 
 }
