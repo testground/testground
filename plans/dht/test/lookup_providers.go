0eaf5ab776f4e939cb18cf320da8ebadbd6d93ed
David Dias <daviddias.p@gmail.com>
2019-10-03 15:00:15 +0300

    create folder for test cases, add stubs for remaining two

---
+++ plans/dht/test/lookup_providers.go
@@ -0,0 +1,13 @@
+package test
+
+import (
+	"fmt"
+
+	"github.com/ipfs/testground/sdk/runtime"
+)
+
+func LookupProviders(runenv *runtime.RunEnv) {
+	fmt.Printf("Not implemented yet")
+
+	runenv.OK()
+}
