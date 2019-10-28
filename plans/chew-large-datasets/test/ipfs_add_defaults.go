1bcf05a86ffe4d5e8c1d22fea2ae5213c0be4fc2
David Dias <daviddias.p@gmail.com>
2019-10-28 11:07:12 +0100

    feat: unpack ipfs add defaults test

--- plans/chew-large-datasets/test/ipfs_add_defaults.go
+++ plans/chew-large-datasets/test/ipfs_add_defaults.go
@@ -1,13 +1,38 @@
 package test
 
 import (
+	"context"
 	"fmt"
+	"os"
 
+	utils "github.com/ipfs/testground/plans/chew-large-datasets/utils"
 	"github.com/ipfs/testground/sdk/runtime"
 )
 
+// IPFS Add Defaults Test
 func IpfsAddDefaults(runenv *runtime.RunEnv) {
-	fmt.Printf("Yo\n")
+	var size int64 = 1024 * 1024
+
+	ctx, _ := context.WithCancel(context.Background())
+	ipfs, err := utils.CreateIpfsInstance(ctx)
+	if err != nil {
+		panic(fmt.Errorf("failed to spawn ephemeral node: %s", err))
+	}
+
+	file := utils.CreateRandomFile(runenv, os.TempDir(), size)
+	defer os.Remove(file.Name())
+
+	unixfsFile, err := utils.GetPathToUnixfsFile(file.Name())
+	if err != nil {
+		panic(fmt.Errorf("failed to get Unixfs file from path: %s", err))
+	}
+
+	cidFile, err := ipfs.Unixfs().Add(ctx, unixfsFile)
+	if err != nil {
+		panic(fmt.Errorf("Could not add File: %s", err))
+	}
+
+	fmt.Printf("Added file to IPFS with CID %s\n", cidFile.String())
 
 	runenv.OK()
 }
