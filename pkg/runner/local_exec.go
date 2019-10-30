efa8c3fda1cf03de37cc093782b90cb0c39d249c
Raúl Kripalani <raul@protocol.ai>
2019-10-30 11:12:50 +0000

    start temporary redis instance if no system redis is detected.

--- pkg/runner/local_exec.go
+++ pkg/runner/local_exec.go
@@ -2,8 +2,10 @@ package runner
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"io"
+	"net"
 	"os/exec"
 	"reflect"
 	"strconv"
@@ -16,22 +18,63 @@ import (
 	"github.com/ipfs/testground/sdk/runtime"
 )
 
+var _ api.Runner = (*LocalExecutableRunner)(nil)
+
 type LocalExecutableRunner struct{}
 
+// LocalExecutableRunnerCfg is the configuration struct for this runner.
 type LocalExecutableRunnerCfg struct{}
-
-var _ api.Runner = (*LocalExecutableRunner)(nil)
 
 func (*LocalExecutableRunner) Run(input *api.RunInput) (*api.RunOutput, error) {
 	var (
-		plan      = input.TestPlan
-		seq       = input.Seq
-		instances = input.Instances
-		name      = plan.Name
+		plan        = input.TestPlan
+		seq         = input.Seq
+		instances   = input.Instances
+		name        = plan.Name
+		redisWaitCh = make(chan struct{})
 	)
+
+	// Housekeeping. If we've started a temporary redis instance for this test,
+	// this defer will keep the runtime alive until it's shut down, giving us an
+	// opportunity to print the "redis stopped successfully" log statement.
+	// Otherwise, it might not be printed out at all.
+	defer func() { <-redisWaitCh }()
 
 	if seq >= len(plan.TestCases) {
 		return nil, fmt.Errorf("invalid sequence number %d for test %s", seq, name)
+	}
+
+	// Check if a local Redis instance is running. If not, try to start it.
+	if _, err := net.Dial("tcp", "localhost:6379"); err == nil {
+		fmt.Println("local redis instance check: OK")
+		close(redisWaitCh)
+	} else {
+		// Try to start a Redis instance.
+		fmt.Println("local redis instance check: FAIL; attempting to start one for this run")
+
+		// This context gets cancelled when the runner has finished, which in
+		// turn signals the temporary Redis instance to shut down.
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		cmd := exec.CommandContext(ctx, "redis-server", "--notify-keyspace-events", "$szxK")
+		if err := cmd.Start(); err == nil {
+			fmt.Println("temporary redis instance started successfully")
+		} else {
+			close(redisWaitCh)
+			return nil, fmt.Errorf("temporary redis instance failed to start: %w", err)
+		}
+
+		// This goroutine monitors the redis instance, and prints a log output
+		// when it's done. The cmd.Wait() returns when the context is cancelled,
+		// which happens when the runner finishes. Once we print the log
+		// statement, we close the redis wait channel, which allows the method
+		// to return.
+		go func() {
+			cmd.Wait()
+			fmt.Println("temporary redis instance stopped successfully")
+			close(redisWaitCh)
+		}()
 	}
 
 	testcase := plan.TestCases[seq]
@@ -46,7 +89,7 @@ func (*LocalExecutableRunner) Run(input *api.RunInput) (*api.RunOutput, error) {
 		TestInstanceParams: input.Parameters,
 	}
 
-	// Spawn as many instances as the test case dictates.
+	// Spawn as many instances as the input parameters require.
 	var wg sync.WaitGroup
 	for i := 0; i < instances; i++ {
 		var env []string
@@ -74,11 +117,11 @@ func (*LocalExecutableRunner) Run(input *api.RunInput) (*api.RunOutput, error) {
 
 			cmd.Env = env
 			cmd.Start()
+			defer cmd.Wait()
 
 			for scanner.Scan() {
 				fmt.Println(a.Index(color, "<< instance "+nstr+" >>"), scanner.Text())
 			}
-
 		}(i)
 	}
 
