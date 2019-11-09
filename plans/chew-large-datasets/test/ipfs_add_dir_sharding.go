73bde8f422d6888623bb5f3c610d9ed97d594c58
David Dias <daviddias.p@gmail.com>
2019-11-09 23:12:04 +0000

    added note and pointer for dir sharing

--- plans/chew-large-datasets/test/ipfs_add_dir_sharding.go
+++ plans/chew-large-datasets/test/ipfs_add_dir_sharding.go
@@ -9,5 +9,7 @@ import (
 func IpfsAddDirSharding(runenv *runtime.RunEnv) {
 	fmt.Printf("Yo - IpfsAddDirSharing")
 
+	// Need https://github.com/ipfs/interface-go-ipfs-core/issues/48
+
 	runenv.OK()
 }
