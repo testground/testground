064810f8a4069b35f49aa68f1a39a0202a79ee18
Henrique Dias <hacdias@gmail.com>
2019-12-04 17:01:36 +0000

    linting

--- plans/chew-large-datasets/test/ipfs_add_defaults.go
+++ plans/chew-large-datasets/test/ipfs_add_defaults.go
@@ -9,8 +9,6 @@ import (
 	"github.com/ipfs/testground/sdk/runtime"
 )
 
-// go build && ./testground run chew-large-datasets/ipfs-add-defaults --runner local:exec --builder exec:go --build-cfg bypass_cache=true  --test-param dir-cfg='[{"depth": 10, "size": "1MB"}, {"depth": 15, "size": "1MB"}]' --test-param file-sizes='["1MB"]'
-
 // IpfsAddDefaults IPFS Add Defaults Test
 func IpfsAddDefaults(runenv *runtime.RunEnv) {
 	ctx, _ := context.WithCancel(context.Background())
@@ -19,7 +17,7 @@ func IpfsAddDefaults(runenv *runtime.RunEnv) {
 		panic(fmt.Errorf("failed to spawn ephemeral node: %s", err))
 	}
 
-	err = utils.ForEachCase(runenv, func (unixfsFile files.Node, isDir bool) error {
+	err = utils.ForEachCase(runenv, func(unixfsFile files.Node, isDir bool) error {
 		t := "file"
 		if isDir {
 			t = "directory"
