87f847fcac51ae4d924ac28222b986edaeccc9b2
Anton Evangelatov <anton.evangelatov@gmail.com>
2019-12-10 14:18:38 +0100

    bump limit on dht test to 10k, fix instance type

--- infra/docker-swarm/terraform/role-worker.tf
+++ infra/docker-swarm/terraform/role-worker.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "worker" {
   count                  = var.workers
   ami                    = data.aws_ami.base.id
   availability_zone      = var.aws_availability_zone
-  instance_type          = var.aws_instance_type_redis
+  instance_type          = var.aws_instance_type_worker
   key_name               = aws_key_pair.key_pair.key_name
   vpc_security_group_ids = ["${aws_security_group.default.id}"]
   subnet_id              = aws_subnet.default.id
