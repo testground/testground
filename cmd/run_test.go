6e59a34e0bd704245e8707dd1d6c6d1d2203d78c
Henrique Dias <hacdias@gmail.com>
2019-12-12 16:07:01 +0100

    refactor: simplify incompatible run test
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

---
+++ cmd/run_test.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/urfave/cli"
+)
+
+func TestIncompatibleRun(t *testing.T) {
+	app := cli.NewApp()
+	app.Name = "testground"
+	app.Commands = Commands
+
+	err := app.Run([]string{
+		"testground",
+		"run",
+		"placebo/ok",
+		"--builder",
+		"exec:go",
+		"--runner",
+		"local:docker",
+	})
+
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestCompatibleRun(t *testing.T) {
+	app := cli.NewApp()
+	app.Name = "testground"
+	app.Commands = Commands
+
+	err := app.Run([]string{
+		"testground",
+		"run",
+		"placebo/ok",
+		"--builder",
+		"exec:go",
+		"--runner",
+		"local:exec",
+	})
+
+	if err != nil {
+		t.Fail()
+	}
+}
