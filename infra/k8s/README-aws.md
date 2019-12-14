cf668834f5c59580fdfe5109ab49ff9a4dbd056a
Anton Evangelatov <anton.evangelatov@gmail.com>
2019-12-14 17:34:08 +0100

    aws eks terraform playbooks (#248)

---
+++ infra/k8s/README-aws.md
@@ -0,0 +1,74 @@
+# Setting up a Kubernetes cluster on AWS for Testground
+
+In this directory, you will find:
+
+```
+» tree
+.
+├── README-aws.md
+└── aws
+    └── terraform          # Playbooks used to setup AWS EKS cluster for Testground - EC2 instances, security groups, networks, etc.
+```
+
+## Requirements
+
+- 1. [Terraform](https://www.terraform.io/).
+
+## Set up infrastructure with Terraform
+
+1. [Configure your AWS credentials](https://docs.aws.amazon.com/cli/)
+
+2. Pick a cluster name. Cluster names must be unique within the same AWS account.
+
+```
+export CLUSTER_NAME=demo
+```
+
+3. Configure the Terraform backend
+
+- Copy `backends/example-backend.tf` to `backends/$CLUSTER_NAME.tf`
+- Update `key` value in `backends/$CLUSTER_NAME.tf`
+
+4. Initialise the Terraform backend
+
+```
+terraform init -backend-config=backends/$CLUSTER_NAME.tf
+```
+
+5. Configure your cluster
+
+- Copy `terraform.tfvars-example` to `terraform.tfvars`
+- Update vars
+
+6. Plan and apply a new cluster with Terraform
+
+```
+terraform plan
+```
+
+```
+terraform apply
+```
+
+7. Update your local .kube config and context
+
+```
+aws eks update-kubeconfig --name $CLUSTER_NAME
+```
+
+8. Edit outputs.tf, add admin users to the config (whoever is going to operate the AWS EKS cluster), and export Terraform outputs
+
+```
+vim config-map-aws-auth
+terraform output config-map-aws-auth > outputs/config-map-aws-auth.yaml
+```
+
+9. Apply config to Kubernetes cluster
+
+```
+kubectl apply -f ./outputs/config-map-aws-auth.yaml
+```
+
+## Teardown
+
+Use `terraform destroy` to remove the cluster from AWS when you are finished working on it.
---
+++ infra/k8s/aws/terraform/eks-cluster.tf
@@ -0,0 +1,79 @@
+# EKS Cluster Resources
+#  * IAM Role to allow EKS service to manage other AWS services
+#  * EC2 Security Group to allow networking traffic with EKS cluster
+#  * EKS Cluster
+
+resource "aws_iam_role" "cluster" {
+  name = "${var.cluster-name}-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+
+}
+
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+resource "aws_security_group" "cluster" {
+  name        = "${var.cluster-name}-cluster"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = aws_vpc.vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    var.default_tags,
+    {
+      "Name" = var.cluster-name
+    },
+  )
+}
+
+resource "aws_security_group_rule" "cluster-ingress-node-https" {
+  description              = "Allow pods to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.cluster.id
+  source_security_group_id = aws_security_group.node-public.id
+  to_port                  = 443
+  type                     = "ingress"
+}
+
+resource "aws_eks_cluster" "cluster" {
+  name     = var.cluster-name
+  role_arn = aws_iam_role.cluster.arn
+
+  vpc_config {
+    security_group_ids = [aws_security_group.cluster.id]
+    subnet_ids         = aws_subnet.subnet.*.id
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.cluster-AmazonEKSServicePolicy,
+  ]
+}
