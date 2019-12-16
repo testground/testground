6d5f88b90f056bc8c7d2e0b5859e0a856ffaeea8
Steven Allen <steven@stebalien.com>
2019-12-16 11:58:55 +0100

    fix: improve sidecar errors on non-linux platforms

--- cmd/sidecar.go
+++ cmd/sidecar.go
@@ -1,16 +1,28 @@
 package cmd
 
 import (
+	"fmt"
+	"runtime"
 	"strings"
 
 	"github.com/ipfs/testground/pkg/sidecar"
 	"github.com/urfave/cli"
 )
 
+var ErrNotLinux = fmt.Errorf("the sidecar only supports linux, not %s", runtime.GOOS)
+
 var SidecarCommand = cli.Command{
 	Name:   "sidecar",
 	Usage:  "runs the sidecar daemon",
 	Action: sidecarCommand,
+	OnUsageError: func(c *cli.Context, err error, isSubcommand bool) error {
+		if runtime.GOOS != "linux" {
+			return ErrNotLinux
+		}
+		_, _ = fmt.Fprintf(c.App.Writer, "%s %s\n\n", "Incorrect Usage.", err.Error())
+		_ = cli.ShowAppHelp(c)
+		return err
+	},
 	Flags: []cli.Flag{
 		cli.GenericFlag{
 			Name:     "runner, r",
@@ -24,5 +36,8 @@ var SidecarCommand = cli.Command{
 }
 
 func sidecarCommand(c *cli.Context) error {
+	if runtime.GOOS != "linux" {
+		return ErrNotLinux
+	}
 	return sidecar.Run(c.String("runner"))
 }
