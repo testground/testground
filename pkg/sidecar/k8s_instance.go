324c510e366e1a518a8792f01c7b95cdb4c3eb44
Anton Evangelatov <anton.evangelatov@gmail.com>
2020-01-16 17:17:23 +0100

    fix routes

--- pkg/sidecar/k8s_instance.go
+++ pkg/sidecar/k8s_instance.go
@@ -120,69 +120,72 @@ func (d *K8sInstanceManager) manageContainer(ctx context.Context, container *doc
 		activeLinks: make(map[string]*k8sLink),
 	}
 
+	// Remove all routes but redis and the data subnet
+
+	// We've found a control network (or some other network).
+	controlLink, err := netlinkHandle.LinkByName("eth0")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get link by name eth0: %w", err)
+	}
+
+	// Get the routes to redis. We need to keep these.
+	redisRoutes, err := netlinkHandle.RouteGet(d.redis)
+	if err != nil {
+		return nil, fmt.Errorf("failed to resolve route to redis: %w", err)
+	}
+
+	if len(redisRoutes) != 1 {
+		return nil, fmt.Errorf("expected to get only one route to redis, but got %v", len(redisRoutes))
+	}
+
+	redisRoute := redisRoutes[0]
+
+	controlLinkRoutes, err := netlinkHandle.RouteList(controlLink, netlink.FAMILY_ALL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list routes for control link %s", controlLink.Attrs().Name)
+	}
+
+	gw := redisRoute.Gw
+
+	podCidr := "100.96.0.0/11"
+
+	// Remove the original routes
+	for _, route := range controlLinkRoutes {
+		if route.Dst != nil && route.Dst.String() == podCidr {
+			fmt.Println("removing route for pod cidr", podCidr)
+			if err := netlinkHandle.RouteDel(&route); err != nil {
+				return nil, fmt.Errorf("failed to delete pod cidr route:", err)
+			}
+
+			continue
+		}
+
+		if route.Dst != nil && route.Dst.Contains(gw) {
+			if err := netlinkHandle.RouteDel(&route); err != nil {
+				return nil, fmt.Errorf("failed to delete route while restricting gw route:", err)
+			}
+
+			route.Dst.IP = gw
+			route.Dst.Mask = net.CIDRMask(32, 32)
+
+			if err := netlinkHandle.RouteAdd(&route); err != nil {
+				return nil, fmt.Errorf("failed to add route while restricting gw route:", err)
+			}
+		}
+
+		if route.Dst == nil && route.Src == nil {
+			if err := netlinkHandle.RouteDel(&route); err != nil {
+				return nil, fmt.Errorf("failed to drop default route:", err)
+			}
+		}
+	}
+
+	// Add specific routes to redis if redis uses this link.
+	if err := netlinkHandle.RouteAdd(&redisRoute); err != nil {
+		return nil, fmt.Errorf("failed to add redis route:", err)
+	}
+
 	return NewInstance(runenv, info.Config.Hostname, network)
-
-	// TODO(anteva): remove all routes but redis and the data subnet
-
-	//reverseIndex := make(map[string]string, len(network.availableLinks))
-	//for name, id := range network.availableLinks {
-	//reverseIndex[id] = name
-	//}
-
-	//// TODO: Some of this code could be factored out into helpers.
-
-	//// Get the routes to redis. We need to keep these.
-	//redisRoutes, err := netlinkHandle.RouteGet(d.redis)
-	//if err != nil {
-	//return nil, fmt.Errorf("failed to resolve route to redis: %w", err)
-	//}
-
-	//for id, link := range links {
-	//if name, ok := reverseIndex[id]; ok {
-	//// manage this network
-	//handle, err := NewNetlinkLink(netlinkHandle, link.Link)
-	//if err != nil {
-	//return nil, fmt.Errorf(
-	//"failed to initialize link %s (%s): %w",
-	//name,
-	//link.Attrs().Name,
-	//err,
-	//)
-	//}
-	//network.activeLinks[name] = &dockerLink{
-	//NetlinkLink: handle,
-	//IPv4:        link.IPv4,
-	//IPv6:        link.IPv6,
-	//}
-	//continue
-	//}
-
-	//// We've found a control network (or some other network).
-
-	//// Get the current routes.
-	//linkRoutes, err := netlinkHandle.RouteList(link, netlink.FAMILY_ALL)
-	//if err != nil {
-	//return nil, fmt.Errorf("failed to list routes for link %s", link.Attrs().Name)
-	//}
-
-	//// Add specific routes to redis if redis uses this link.
-	//for _, route := range redisRoutes {
-	//if route.LinkIndex != link.Attrs().Index {
-	//continue
-	//}
-	//if err := netlinkHandle.RouteAdd(&route); err != nil {
-	//return nil, fmt.Errorf("failed to add new route: %w", err)
-	//}
-	//break
-	//}
-
-	//// Remove the original routes
-	//for _, route := range linkRoutes {
-	//if err := netlinkHandle.RouteDel(&route); err != nil {
-	//return nil, fmt.Errorf("failed to remove existing route: %w", err)
-	//}
-	//}
-	//}
 }
 
 type k8sLink struct {
@@ -321,72 +324,20 @@ func (n *K8sNetwork) ConfigureNetwork(ctx context.Context, cfg *sync.NetworkConf
 }
 
 func (n *K8sNetwork) ListActive() []string {
-	panic("not implemented yet")
-	return []string{}
+	networks := make([]string, 0, len(n.activeLinks))
+	for name := range n.activeLinks {
+		networks = append(networks, name)
+	}
+	return networks
 }
 
 func (n *K8sNetwork) ListAvailable() []string {
 	panic("not implemented yet")
-	return []string{}
 }
 
-//func attach(addr string, netns string, containerID string, ifName string) error {
-//// <addr>     = [ip:]<cidr> | net:<cidr> | net:default
-
-//// addr := "net:10.36.79.0/24"
-//// ip := "10.36.79.10/24"
-//// netns := "" // abs path
-//// containerID := ""
-//// ifName := "net1"
-
-//// we assume that this is mapped as a volume
-//cniBinPath := filepath.SplitList("/host/opt/cni/bin")
-
-//bytes := []byte(`
-//{
-//"cniVersion": "0.3.0",
-//"name": "weave",
-//"plugins": [
-//{
-//"name": "weave",
-//"type": "weave-net",
-//"ipam": {
-//"subnet": "` + addr + `"
-//},
-//"hairpinMode": true
-//}
-//]
-//}
-//`)
-
-//netconf, err := libcni.ConfListFromBytes(bytes)
-//if err != nil {
-//return err
-//}
-
-//cniArgs := [][2]string{}                   // empty
-//capabilityArgs := map[string]interface{}{} // empty
-
-//cninet := libcni.NewCNIConfig(cniBinPath, nil)
-
-//rt := &libcni.RuntimeConf{
-//ContainerID:    containerID,
-//NetNS:          netns,
-//IfName:         ifName,
-//Args:           cniArgs,
-//CapabilityArgs: capabilityArgs,
-//}
-
-//_, err = cninet.AddNetworkList(context.TODO(), netconf, rt)
-//if err != nil {
-//return err
-//}
-
-//return nil
-//}
-
 func newNetworkConfigList(t string, addr string) (*libcni.NetworkConfigList, error) {
-	if t == "net" {
+	switch t {
+	case "net":
 		bytes := []byte(`
 {
 		"cniVersion": "0.3.0",
@@ -404,9 +355,8 @@ func newNetworkConfigList(t string, addr string) (*libcni.NetworkConfigList, err
 }
 `)
 		return libcni.ConfListFromBytes(bytes)
-	}
 
-	if t == "ip" {
+	case "ip":
 		bytes := []byte(`
 {
 		"cniVersion": "0.3.0",
@@ -429,7 +379,8 @@ func newNetworkConfigList(t string, addr string) (*libcni.NetworkConfigList, err
 }
 `)
 		return libcni.ConfListFromBytes(bytes)
-	}
 
-	return nil, errors.New("unknown type")
+	default:
+		return nil, errors.New("unknown type")
+	}
 }
