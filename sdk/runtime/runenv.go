ecb2e0c58a35f95829a1e384004906e0f1708064
Adin Schmahmann <adin.schmahmann@gmail.com>
2020-01-22 15:48:21 -0800

    dht: add undialable nodes

--- sdk/runtime/runenv.go
+++ sdk/runtime/runenv.go
@@ -307,6 +307,21 @@ func (re *RunParams) IntParam(name string) int {
 	return i
 }
 
+// FloatParam returns a float64 parameter, or -1.0 if the parameter is not set or
+// the conversion failed. It panics on error.
+func (re *RunEnv) FloatParam(name string) float64 {
+	v, ok := re.TestInstanceParams[name]
+	if !ok {
+		return -1.0
+	}
+
+	f, err := strconv.ParseFloat(v, 32)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
 // BooleanParam returns the Boolean value of the parameter, or false if not passed
 func (re *RunParams) BooleanParam(name string) bool {
 	s := re.TestInstanceParams[name]
