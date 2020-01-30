39cfb332aa85ca887612d8a6df9b3484ac1c8204
Adin Schmahmann <adin.schmahmann@gmail.com>
2020-01-30 16:30:57 -0500

    added mesh bootstrapping

--- plans/dht/graphs.ps1
+++ plans/dht/graphs.ps1
@@ -1,25 +1,23 @@
 param($graphID,$dataDir)
 
 $allGraphs = @("br", "bt", "at")
+$fmt = "png"
 
 if (!$dataDir) {
 	$dataDir = gci $HOME/.testground/results/dht | Sort LastWriteTime | select -Last 1
 }
 
-if (!$graphID) {
-	$allGraphs | %{
-		$g = $_
-		$links = gci $dataDir -recurse | ?{$_.Name -eq "stderr.json"} | Get-Content |
-		ConvertFrom-Json | ?{$_.N -eq "Graph.$g"} | %{$_.M} | ConvertFrom-Json |
-		%{$_.From+" -> "+$_.To+";"}
-		
-		"digraph D { `n" + $links + "`n }" | circo -Tsvg -o "$g.svg"
-	}
-} else {
-	$g = $graphID
+if ($graphID) {
+	$allGraphs = @($graphID)
+}
+
+$allGraphs | %{
+	$g = $_
 	$links = gci $dataDir -recurse | ?{$_.Name -eq "stderr.json"} | Get-Content |
-		ConvertFrom-Json | ?{$_.N -eq "Graph.$g"} | %{$_.M} | ConvertFrom-Json |
-		%{$_.From+" -> "+$_.To+";"}
-		
-		"digraph D { `n" + $links + "`n }" | circo -Tsvg -o "$g.svg"
+	ConvertFrom-Json | ?{$_.N -eq "Graph.$g"} | %{$_.M} | ConvertFrom-Json |
+	%{$_.From+" -> "+$_.To+";"}
+	
+	$file = "digraph D { `n" + $links + "`n }"
+	$file > "$g.dot"
+	$file | sfdp -x -Goverlap=scale "-T$fmt" -o "$g.$fmt"
 }
