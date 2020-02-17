59af215ee582ea882c1747a53a6d78d1b0fd0747
Cory Schwartz <cory@protocol.ai>
2020-02-16 17:11:30 -0800

    Add network utils
    This is a generalized form of the network util from the bitswap-tuining plan
    
    License: MIT
    Signed-off-by: Cory Schwartz <cory@protocol.ai>

---
+++ sdk/utils/network.go
@@ -0,0 +1,68 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/ipfs/testground/sdk/runtime"
+	"github.com/ipfs/testground/sdk/sync"
+)
+
+// SetupNetwork instructs the sidecar (if enabled) to setup the network for this
+// test case. The configuration for this function relies on a few test params, which should be
+// provided by the relevant toml file. When successful, this method returns the applied bandwidth
+// and enters a configured state for the applied network.
+//
+// toml configuration:
+//  bandwidth_mb
+//  latency_ms
+func SetupNetwork(ctx context.Context, runenv *runtime.RunEnv, watcher *sync.Watcher, writer *sync.Writer, network string) (time.Duration, int, error) {
+	if !runenv.TestSidecar {
+		return 0, 0, nil
+	}
+	// Wait for the network to be initialized.
+	if err := sync.WaitNetworkInitialized(ctx, runenv, watcher); err != nil {
+		return 0, 0, err
+	}
+	// TODO: just put the unique testplan id inside the runenv?
+	hostname, err := os.Hostname()
+	if err != nil {
+		return 0, 0, err
+	}
+	latency := time.Duration(runenv.IntParam("latency_ms")) * time.Millisecond
+	if err != nil {
+		return 0, 0, err
+	}
+	bandwidth := runenv.IntParam("bandwidth_mb")
+	_, err = writer.Write(ctx, sync.NetworkSubtree(hostname), &sync.NetworkConfig{
+		Network: network,
+		Enable:  true,
+		Default: sync.LinkShape{
+			Latency:   latency,
+			Bandwidth: uint64(bandwidth) * 1024 * 1024,
+		},
+		State: sync.State(fmt.Sprintf("%s-configurd", network)),
+	})
+	if err != nil {
+		return 0, 0, fmt.Errorf("failed to configure network: %w", err)
+	}
+	runenv.RecordMessage("Configured Network latency: %d bandwidth: %d", latency, bandwidth)
+
+	return latency, bandwidth, nil
+}
+
+// Configure the network and wait for all instances to to complete the network setup also.
+func SetupNetworkWaitAll(ctx context.Context, runenv *runtime.RunEnv, watcher *sync.Watcher, writer *sync.Writer, network string) (time.Duration, int, error) {
+	latency, bandwidth, err := SetupNetwork(ctx, runenv, watcher, writer, network)
+	if err != nil {
+		runenv.RecordFailure(err)
+		return latency, bandwidth, err
+	}
+	err = <-watcher.Barrier(ctx, "network-configured", int64(runenv.TestInstanceCount))
+	if err != nil {
+		return 0, 0, fmt.Errorf("failed to configure network: %w", err)
+	}
+	return latency, bandwidth, nil
+}
