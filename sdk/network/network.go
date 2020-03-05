68f00fbe169da38fd6cba55c3cab8366437d4322
Cory Schwartz <cory@protocol.ai>
2020-03-04 22:23:40 -0800

    update WaitNetworkInitialized
    License: MIT
    Signed-off-by: Cory Schwartz <cory@protocol.ai>

--- sdk/network/network.go
+++ sdk/network/network.go
@@ -10,6 +10,27 @@ import (
 	"github.com/ipfs/testground/sdk/sync"
 )
 
+const (
+	// magic values that we monitor on the Testground runner side to detect when Testground
+	// testplan instances are initialised and at the stage of actually running a test
+	// check cluster_k8s.go for more information
+	NetworkInitialisationSuccessful = "network initialisation successful"
+	NetworkInitialisationFailed     = "network initialisation failed"
+)
+
+// WaitNetworkInitialized waits for the sidecar to initialize the network, if the sidecar is enabled.
+func WaitNetworkInitialized(ctx context.Context, runenv *runtime.RunEnv, watcher *sync.Watcher) error {
+	if runenv.TestSidecar {
+		err := <-watcher.Barrier(ctx, "network-initialized", int64(runenv.TestInstanceCount))
+		if err != nil {
+			runenv.RecordMessage(NetworkInitialisationFailed)
+			return fmt.Errorf("failed to initialize network: %w", err)
+		}
+	}
+	runenv.RecordMessage(NetworkInitialisationSuccessful)
+	return nil
+}
+
 // SetupNetwork instructs the sidecar (if enabled) to setup the network for this
 // test case. The configuration for this function relies on a few test params, which should be
 // provided by the relevant toml file. When successful, this method returns the applied bandwidth
@@ -64,14 +85,4 @@ func SetupNetworkWaitAll(ctx context.Context, runenv *runtime.RunEnv, watcher *s
 		return 0, 0, fmt.Errorf("failed to configure network: %w", err)
 	}
 	return latency, bandwidth, nil
-}
-
-func WaitNetworkInitialized(ctx context.Context, runenv *runtime.RunEnv, watcher *sync.Watcher) error {
-	if runenv.TestSidecar {
-		err := <-watcher.Barrier(ctx, "network-initialized", int64(runenv.TestInstanceCount))
-		if err != nil {
-			return fmt.Errorf("failed to initialize network: %w", err)
-		}
-	}
-	return nil
 }
