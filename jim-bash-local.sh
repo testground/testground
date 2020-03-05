4174309a0bc2e54f797452ceccd9db11d4a068f6
Jim Pick <jim@protocol.ai>
2020-03-05 05:25:25 +0000

    New helper dev scripts

---
+++ jim-bash-local.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+CHECK=$(docker ps --format "{{.Names}}" | grep ^tg- | perl -pe 's/^tg-(.*)-([^-]+)-single-\d+$/\1 \2/' | sort | uniq | wc -l)
+
+if [ "$CHECK" -ne "1" ]; then
+	echo "Too many or two few tg deploys: $CHECK"
+	exit 1
+fi
+BASE=$(docker ps --format "{{.Names}}" | grep ^tg- | perl -pe 's/^tg-(.*)-([^-]+)-single-\d+$/\1/' | sort | uniq)
+ID=$(docker ps --format "{{.Names}}" | grep ^tg- | perl -pe 's/^tg-(.*)-([^-]+)-single-\d+$/\2/' | sort | uniq)
+echo $BASE
+echo $ID
+
+if [ -z "$1" ]; then
+	echo "Need to specific instance number"
+	exit 1
+fi
+
+exec docker exec -it tg-$BASE-$ID-single-$1 /bin/bash
+
