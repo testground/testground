d7c2ec43d9a07dccfdf714313c48685081cad616
Jim Pick <jim@protocol.ai>
2020-03-06 04:15:24 +0000

    Add checkout script

---
+++ plans/lotus-js/checkout.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Proof-of-concept / playground
+git clone git@github.com:jimpick/lotus-api-playground.git
+
+# js-lotus-api
+# js-lotus-api-build
+# js-lotus-api-format
+# js-lotus-api-provider-browser
+# js-lotus-api-provider-mock
+# js-lotus-api-provider-nodejs
+# js-lotus-api-query
+# js-lotus-api-rpc
+# js-lotus-api-schema
+# js-lotus-api-testground
+
