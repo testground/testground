3de70746d7aa0e5d6900fb9246185e7825dcd070
Raúl Kripalani <raul@protocol.ai>
2020-03-10 19:43:43 +0000

    baseline test: fix minor issues (+).
    * yield from the pusher goroutine when closing.
    * barrier test case: make iteration count a test param.
    * barrier test case: anonymous type for config.

--- plans/benchmarks/benchmarks.go
+++ plans/benchmarks/benchmarks.go
@@ -12,31 +12,24 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-type barrierBenchCfg struct {
-	Name    string
-	Gauge   prometheus.Gauge
-	Percent float64
-}
-
 // This method emits the time as output. It does *not* emit a prometheus metric.
 func emitTime(runenv *runtime.RunEnv, name string, duration time.Duration) {
 	runenv.RecordMetric(&runtime.MetricDefinition{
 		Name:           name,
-		Unit:           "Seconds",
+		Unit:           "seconds",
 		ImprovementDir: -1,
-	},
-		duration.Seconds())
+	}, duration.Seconds())
 }
 
 // StartTimeBench does nothing but start up and report the time it took to start.
 // This relies on the testground daemon to inject the time when the plan is scheduled
 // into the runtime environment
 func StartTimeBench(runenv *runtime.RunEnv) error {
-	scheduledTime := runenv.TestStartTime
-	duration := time.Since(scheduledTime)
-	emitTime(runenv, "Time to Start", duration)
+	elapsed := time.Since(runenv.TestStartTime)
+	emitTime(runenv, "Time to start", elapsed)
+
 	gauge := runtime.NewGauge(runenv, "start_time", "time from plan scheduled to plan booted")
-	gauge.Set(float64(duration))
+	gauge.Set(float64(elapsed))
 	return nil
 }
 
@@ -44,9 +37,15 @@ func StartTimeBench(runenv *runtime.RunEnv) error {
 // The metric it emits represents the time between plan start and when the network initialization
 // is completed.
 func NetworkInitBench(runenv *runtime.RunEnv) error {
+	// FIX(cory/raulk) this test will not work with local:exec, because it
+	// doesn't support the sidecar yet. We should probably skip it
+	// conditionally, based on the runner. We might want to inject the runner
+	// name in the runenv, so tests like this can modify their behaviour
+	// accordingly.
 	startupTime := time.Now()
 	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Second)
 	defer cancel()
+
 	watcher, writer := sync.MustWatcherWriter(ctx, runenv)
 	defer watcher.Close()
 	defer writer.Close()
@@ -55,16 +54,24 @@ func NetworkInitBench(runenv *runtime.RunEnv) error {
 		return err
 	}
 
-	duration := time.Since(startupTime)
-	emitTime(runenv, "Time to Network", duration)
+	elapsed := time.Since(startupTime)
+	emitTime(runenv, "Time to network init", elapsed)
+
 	gauge := runtime.NewGauge(runenv, "net_init_time", "Time waiting for network initialization")
-	gauge.Set(float64(duration))
+	gauge.Set(float64(elapsed))
 	return nil
 }
 
 func NetworkLinkShapeBench(runenv *runtime.RunEnv) error {
+	// FIX(cory/raulk) this test will not work with local:exec, because it
+	// doesn't support the sidecar yet. We should probably skip it
+	// conditionally, based on the runner. We might want to inject the runner
+	// name in the runenv, so tests like this can modify their behaviour
+	// accordingly.
+
 	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Second)
 	defer cancel()
+
 	watcher, writer := sync.MustWatcherWriter(ctx, runenv)
 	defer watcher.Close()
 	defer writer.Close()
@@ -72,7 +79,11 @@ func NetworkLinkShapeBench(runenv *runtime.RunEnv) error {
 	if err := sync.WaitNetworkInitialized(ctx, runenv, watcher); err != nil {
 		return err
 	}
+
 	// A state name unique to the container...
+	//
+	// FIX(cory/raulk): this name is not unique in local:exec; it will be the
+	// host's name.
 	name, err := os.Hostname()
 	if err != nil {
 		return err
@@ -89,6 +100,7 @@ func NetworkLinkShapeBench(runenv *runtime.RunEnv) error {
 	}
 
 	beforeNetConfig := time.Now()
+
 	// Send configuration to the sidecar.
 	_, err = writer.Write(ctx, sync.NetworkSubtree(name), &netConfig)
 	if err != nil {
@@ -107,11 +119,14 @@ func NetworkLinkShapeBench(runenv *runtime.RunEnv) error {
 	return nil
 }
 
-// BarrierBench tests the time it takes to wait on Barriers, waiting on a different number
-// of instances in each loop.
+// BarrierBench tests the time it takes to wait on Barriers, waiting on a
+// different number of instances in each loop.
 func BarrierBench(runenv *runtime.RunEnv) error {
+	iterations := runenv.IntParam("iterations")
+
 	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Second)
 	defer cancel()
+
 	watcher, writer := sync.MustWatcherWriter(ctx, runenv)
 	defer watcher.Close()
 	defer writer.Close()
@@ -120,11 +135,16 @@ func BarrierBench(runenv *runtime.RunEnv) error {
 		return err
 	}
 
-	var tests []*barrierBenchCfg
+	type cfg struct {
+		Name    string
+		Gauge   prometheus.Gauge
+		Percent float64
+	}
 
+	var tests []*cfg
 	for percent := 0.2; percent <= 1.0; percent += 0.2 {
 		name := fmt.Sprintf("barrier_time_%d_percent", int(percent*100))
-		t := barrierBenchCfg{
+		t := cfg{
 			Name:    name,
 			Gauge:   runtime.NewGauge(runenv, name, fmt.Sprintf("time waiting for %f barrier", percent)),
 			Percent: percent,
@@ -134,27 +154,33 @@ func BarrierBench(runenv *runtime.RunEnv) error {
 
 	// Loop a bunch of times to generate a good amount of data.
 	// The number of loops is pretty arbitrary here.
-	for i := 0; i <= 100000; i++ {
+	for i := 1; i <= iterations; i++ {
 		for _, tst := range tests {
 			readyState := sync.State(fmt.Sprintf("ready_%d_%s", i, tst.Name))
 			testState := sync.State(fmt.Sprintf("test_%d_%s", i, tst.Name))
 			testInstanceNum := int64(math.Floor(float64(runenv.TestInstanceCount) * tst.Percent))
+
 			if testInstanceNum == 0.0 {
 				testInstanceNum = 1.0
 			}
+
 			_, err := writer.SignalEntry(ctx, readyState)
 			if err != nil {
 				return err
 			}
+
 			<-watcher.Barrier(ctx, readyState, int64(runenv.TestInstanceCount))
+
 			barrierTestStart := time.Now()
 			_, err = writer.SignalEntry(ctx, testState)
 			if err != nil {
 				return err
 			}
 			<-watcher.Barrier(ctx, sync.State(testState), testInstanceNum)
+
 			duration := time.Since(barrierTestStart)
 			emitTime(runenv, tst.Name, duration)
+
 			// I picked `Add` here instead of `Set` so the measurement will have to be rated.
 			// The reason I did this is so the rate will drop to zero after the end of the test
 			// Use rate(barrier_time_XX_percent) in prometheus graphs.
