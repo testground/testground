6458d23987dd6369738ac2310447c775a6a76428
Cory Schwartz <cory@protocol.ai>
2020-03-09 20:48:35 -0700

    Subtree bench
    License: MIT
    Signed-off-by: Cory Schwartz <cory@protocol.ai>

--- plans/benchmarks/benchmarks.go
+++ plans/benchmarks/benchmarks.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"math"
+	"math/rand"
 	"os"
+	"reflect"
 	"time"
 
 	"github.com/ipfs/testground/sdk/runtime"
@@ -16,6 +18,12 @@ type barrierBenchCfg struct {
 	Name    string
 	Gauge   prometheus.Gauge
 	Percent float64
+}
+
+type subtreeBenchCfg struct {
+	Name  string
+	Gauge prometheus.Gauge
+	Data  []byte
 }
 
 // This method emits the time as output. It does *not* emit a prometheus metric.
@@ -162,5 +170,103 @@ func BarrierBench(runenv *runtime.RunEnv) error {
 		}
 	}
 
+	return nil
+}
+
+func SubtreePublishBench(runenv *runtime.RunEnv) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Second)
+	defer cancel()
+	watcher, writer := sync.MustWatcherWriter(ctx, runenv)
+	defer watcher.Close()
+	defer writer.Close()
+
+	if err := sync.WaitNetworkInitialized(ctx, runenv, watcher); err != nil {
+		return err
+	}
+
+	var msg []byte
+	st := sync.Subtree{
+		GroupKey:    "messages",
+		PayloadType: reflect.TypeOf(&msg),
+		KeyFunc: func(val interface{}) string {
+			return string(*val.(*[]byte))
+		}}
+
+	seq, err := writer.Write(ctx, &st, runenv.TestRun)
+	if err != nil {
+		return err
+	}
+
+	// Create several tests up to 1MB
+	// Anything over 1500 is likely to have ethernet fragmentation
+	// Do these sizes make any sense at all? I really don't know.
+	var tests []*subtreeBenchCfg
+	for size := 64; size <= 1024*1024; size = size << 1 {
+		buf := make([]byte, size)
+		name := fmt.Sprintf("subtree_bench_%s_bytes", string(size))
+		t := subtreeBenchCfg{
+			Name:  name,
+			Gauge: runtime.NewGauge(runenv, name, fmt.Sprintf("Time to transmmit %d bytes", size)),
+			Data:  buf,
+		}
+		tests = append(tests, &t)
+
+	}
+
+	if seq == 1 {
+		// Publish information to the subtree and wait for all to receive it.
+		return subtreePublisher(ctx, runenv, writer, watcher, &st, tests)
+	} else {
+		// Subscribe to the subtree as quickly as quickly as possible.
+		return subtreeSubscriber(ctx, runenv, writer, watcher, &st, tests)
+	}
+}
+
+func subtreePublisher(ctx context.Context, runenv *runtime.RunEnv, writer *sync.Writer, watcher *sync.Watcher, st *sync.Subtree, tests []*subtreeBenchCfg) error {
+
+	rand.Seed(time.Now().UnixNano())
+
+	// The sender doesn't publish metrics.
+	// Just keep everyone on the same page, and publsh fresh data.
+	// Wait until all are ready,
+	// publish
+	// begin the test
+	// Repeat.
+
+	for i := 0; i <= 100000; i++ {
+		for _, tst := range tests {
+			readyState := sync.State(fmt.Sprintf("ready_%d_%s", i, tst.Name))
+			testState := sync.State(fmt.Sprintf("test_%d_%s", i, tst.Name))
+			rand.Read(tst.Data)
+			_, err := writer.SignalEntry(ctx, readyState)
+			if err != nil {
+				return err
+			}
+			<-watcher.Barrier(ctx, readyState, int64(runenv.TestInstanceCount))
+			writer.SignalEntry(ctx, testState)
+			writer.Write(ctx, st, tst.Data)
+		}
+	}
+	return nil
+}
+
+// Subscribers signal they are ready, and then wait until there is one test signal.
+// Once they see that signal, measure the time between the test signal entry and data
+// is retrieved from the subscription channel.
+func subtreeSubscriber(ctx context.Context, runenv *runtime.RunEnv, writer *sync.Writer, watcher *sync.Watcher, st *sync.Subtree, tests []*subtreeBenchCfg) error {
+
+	for i := 0; i <= 100000; i++ {
+		for _, tst := range tests {
+			readyState := sync.State(fmt.Sprintf("ready_%d_%s", i, tst.Name))
+			testState := sync.State(fmt.Sprintf("test_%d_%s", i, tst.Name))
+			writer.SignalEntry(ctx, readyState)
+			_ = <-watcher.Barrier(ctx, testState, int64(1))
+			ch := make(chan []byte, 1)
+			timeStart := time.Now()
+			watcher.Subscribe(ctx, st, ch)
+			<-ch
+			tst.Gauge.Set(float64(time.Since(timeStart).Milliseconds()))
+		}
+	}
 	return nil
 }
