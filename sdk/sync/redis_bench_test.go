c3837021b22b9703fe629fc8c2a87518b0f4b86c
Raúl Kripalani <raul@protocol.ai>
2020-03-17 18:37:51 +0000

    add benchmark for sync barriers.

---
+++ sdk/sync/redis_bench_test.go
@@ -0,0 +1,44 @@
+package sync
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func BenchmarkBarrier(b *testing.B) {
+	b.ReportAllocs()
+
+	close := ensureRedis(b)
+	defer close()
+
+	runenv := randomRunEnv()
+
+	for n := 0; n < b.N; n++ {
+		state := State(fmt.Sprintf("yoda-%d", n))
+
+		// simulate 50000 instances, each signalling entry, and waiting for all others
+		total := 50000
+
+		var wg sync.WaitGroup
+		for i := 0; i < total; i++ {
+			wg.Add(1)
+
+			go func() {
+				defer wg.Done()
+
+				watcher, writer := MustWatcherWriter(context.Background(), runenv)
+				defer watcher.Close()
+				defer writer.Close()
+
+				ctx, cancel := context.WithCancel(context.Background())
+				defer cancel()
+
+				writer.SignalEntry(ctx, state)
+				<-watcher.Barrier(ctx, state, int64(total))
+			}()
+		}
+		wg.Wait()
+	}
+}
