498ad8c434e77464404df4996d9270e44e0ccb21
Cory Schwartz <cory@protocol.ai>
2020-03-18 14:27:56 -0700

    merge merge merge

--- plans/dht/main.go
+++ plans/dht/main.go
@@ -5,11 +5,12 @@ import (
 	"github.com/ipfs/testground/sdk/runtime"
 )
 
-var testCases = []func(*runtime.RunEnv) error{
-	test.FindPeers,
-	test.FindProviders,
-	test.ProvideStress,
-	test.StoreGetValue,
+var testCases = map[string]func(*runtime.RunEnv) error{
+	"find-peers": test.FindPeers,
+	"find-providers" : test.FindProviders,
+	"provide-stress" : test.ProvideStress,
+	"store-get-value" : test.StoreGetValue,
+	"barrier" : test.Barrier,
 }
 
 func main() {
@@ -20,5 +21,5 @@ func run(runenv *runtime.RunEnv) error {
 	if runenv.TestCaseSeq < 0 {
 		panic("test case sequence number not set")
 	}
-	return testCases[runenv.TestCaseSeq](runenv)
+	return testCases[runenv.TestCase](runenv)
 }
