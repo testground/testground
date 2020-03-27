f57cdd8f359513029c8100f3a53e9d570cb73a17
Cory Schwartz <cory@protocol.ai>
2020-03-27 15:17:07 -0700

    ci.ID -> opts.ContainerName

--- pkg/docker/container.go
+++ pkg/docker/container.go
@@ -72,7 +72,7 @@ func EnsureContainer(ctx context.Context, log *zap.SugaredLogger, cli *client.Cl
 
 		err := cli.ContainerStart(ctx, ci.ID, types.ContainerStartOptions{})
 		if err != nil {
-			log.Errorf("starting container failed", "container_id %s: %v", ci.ID, err)
+			log.Errorf("starting container failed", "container_id %s: %v", opts.ContainerName, err)
 			return nil, false, err
 		}
 
