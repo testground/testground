b9066a122e433029bdb4fb6c95ce4964d6d0592c
Anton Evangelatov <anton.evangelatov@gmail.com>
2020-04-14 13:00:41 +0200

    remove s3fs

--- infra/k8s/s3fs/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY}" > /etc/passwd-s3fs 
-chmod 0400 /etc/passwd-s3fs
-
-/usr/local/bin/s3fs $S3_BUCKET $MNT_POINT -f -o url=${S3_ENDPOINT},allow_other,use_cache=/tmp,max_stat_cache_size=1000,stat_cache_expire=900,retries=5,connect_timeout=10${S3_EXTRAVARS}
