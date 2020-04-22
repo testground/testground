322aa3c33d89d52c63583f484d2e05a834255d8a
Raúl Kripalani <raul.kripalani@gmail.com>
2020-04-22 17:49:16 +0100

    migrate dht test plan to libp2p/test-plans. (#892)

--- plans/dht/test/sync.go
+++
@@ -1,9 +0,0 @@
-package test
-
-import (
-	"github.com/testground/sdk-go/sync"
-)
-
-// PeerAttribTopic represents a subtree under the test run's sync tree where peers
-// participating in this distributed test advertise their attributes.
-var PeerAttribTopic = sync.NewTopic("attribs", &DHTNodeInfo{})
