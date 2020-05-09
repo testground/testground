1e46a8d0dc88c9a454737f99a39af15e9626ca96
Jim Pick <jim@protocol.ai>
2020-05-09 20:18:50 +0000

    New build, v26 proofs!

--- jim-prune-images.sh
+++ jim-prune-images.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker images | awk '{print $1}' | grep '^[0-9]' | xargs docker rmi
-#docker images | awk '{print $1}' | grep '^[0-9]' 
+docker images | awk '{print $1 ":" $2}' | grep '^[0-9]' | xargs docker rmi
+#docker images | awk '{print $1 ":" $2}'
