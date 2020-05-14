926ab443f501bc5657d63af8bc7b630d4224484d
Jim Pick <jim@protocol.ai>
2020-05-14 21:42:18 +0000

    New builds

--- plans/lotus-base-testnet-v3-2020.03.19/jim-build.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-../../testground --vv build single lotus-base-testnet-v3-2020.03.19/placeholder \
-    --builder=docker:lotus
--- plans/lotus-js-testnet-v3-2020.04.14/monitor.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-watch -n 5 "lotus chain list --count=1; lotus-storage-miner info; echo --- Node; cat /outputs/node.out | grep -v 'New heaviest\|insecure test\|new block\|New block\|scheduling incoming' | tail -10 ; echo --- Miner; cat /outputs/miner.out | tail -10"
-
