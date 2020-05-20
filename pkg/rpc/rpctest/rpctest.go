1d424e91fdb0558dc44b5961d5c93eab60d09d31
Cory Schwartz <cory@protocol.ai>
2020-05-20 03:39:52 -0700

    dont use naked return

--- pkg/rpc/rpctest/rpctest.go
+++ pkg/rpc/rpctest/rpctest.go
@@ -11,7 +11,5 @@ import (
 func NewRecordedOutputWriter(req_id string) (rec *httptest.ResponseRecorder, ow *rpc.OutputWriter) {
 	req := httptest.NewRequest("GET", "/", strings.NewReader(""))
 	req.Header.Add("X-Request-ID", req_id)
-	rec = httptest.NewRecorder()
-	ow = rpc.NewOutputWriter(rec, req)
-	return
+	return httptest.NewRecorder(), rpc.NewOutputWriter(rec, req)
 }
