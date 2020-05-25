b2483437b44b58599d1555b4df9f6a05d75ea909
Anton Evangelatov <anton.evangelatov@gmail.com>
2020-05-25 18:27:19 +0200

    add network/ping-pong test for sidecar

---
+++ integration_tests/06_docker_network_ping-pong.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+my_dir="$(dirname "$0")"
+source "$my_dir/header.sh"
+
+testground plan import --from plans/network
+testground build single --builder docker:go --plan network | tee build.out
+export ARTIFACT=$(awk -F\" '/generated build artifact/ {print $8}' build.out)
+docker tag $ARTIFACT testplan:network
+
+pushd $TEMPDIR
+testground healthcheck --runner local:docker --fix
+testground run single --runner local:docker --builder docker:go --use-build testplan:network --instances 2 --plan network --testcase ping-pong --collect | tee run.out
+RUNID=$(awk '/finished run with ID/ { print $9 }' run.out)
+echo "checking run $RUNID"
+file $RUNID.tgz
+unzip $RUNID.tgz
+SIZEOUT=$(cat ./"$RUNID"/single/0/run.out | wc -c)
+echo "run.out is $SIZEOUT bytes."
+SIZEERR=$(cat ./"$RUNID"/single/0/run.err | wc -c)
+test $SIZEOUT -gt 0 && test $SIZEERR -eq 0
+popd
