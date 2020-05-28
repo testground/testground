62843351c7f50cb5388e0103d0eca1503be0c084
Jim Pick <jim@protocol.ai>
2020-05-28 22:33:22 +0000

    New builds, IPFS support

--- jim-monitor-k8s.sh
+++ jim-monitor-k8s.sh
@@ -1,20 +1,23 @@
 #! /bin/bash
 
-CHECK=$(kubectl get pods | grep ^tg- | awk '{print $1}' | perl -pe 's/^tg-(.*)-([^-]+)-single-\d+$/\1 \2/' | sort | uniq | wc -l)
-
-if [ "$CHECK" -ne "1" ]; then
-	echo "Too many or two few tg deploys: $CHECK"
-	exit 1
-fi
-BASE=$(kubectl get pods | grep ^tg- | awk '{print $1}' | perl -pe 's/^tg-(.*)-([^-]+)-single-\d+$/\1/' | sort | uniq)
-ID=$(kubectl get pods | grep ^tg- | awk '{print $1}' | perl -pe 's/^tg-(.*)-([^-]+)-single-\d+$/\2/' | sort | uniq)
-echo $BASE
-echo $ID
-
 if [ -z "$1" ]; then
-	echo "Need to specific instance number"
-	exit 1
+  echo "Need to specific instance number"
+  exit 1
 fi
 
-kubectl exec -it tg-$BASE-$ID-single-$1 -- monitor.sh
+while true; do
+  CHECK=$(kubectl get pods | grep ^tg- | awk '{print $1}' | perl -pe 's/^tg-(.*)-([^-]+)-single-\d+$/\1 \2/' | sort | uniq | wc -l)
+
+  if [ "$CHECK" -ne "1" ]; then
+    echo "Too many or two few tg deploys: $CHECK"
+  else
+    BASE=$(kubectl get pods | grep ^tg- | awk '{print $1}' | perl -pe 's/^tg-(.*)-([^-]+)-single-\d+$/\1/' | sort | uniq)
+    ID=$(kubectl get pods | grep ^tg- | awk '{print $1}' | perl -pe 's/^tg-(.*)-([^-]+)-single-\d+$/\2/' | sort | uniq)
+    echo $BASE
+    echo $ID
+
+    kubectl exec -it tg-$BASE-$ID-single-$1 -- monitor.sh
+  fi
+  sleep 5
+done
 
---
+++ plans/lotus-js-master-2020.05.28/go.mod
@@ -0,0 +1,22 @@
+module github.com/ipfs/testground/plans/lotus-js
+
+go 1.13
+
+require (
+	github.com/filecoin-project/go-address v0.0.2-0.20200504173055-8b6f2fb2b3ef
+	github.com/filecoin-project/go-jsonrpc v0.1.1-0.20200520183639-7c6ee2e066b4
+	github.com/filecoin-project/lotus v0.2.10
+	github.com/ipfs/testground/sdk/runtime v0.1.0
+	github.com/ipfs/testground/sdk/sync v0.1.0
+	github.com/libp2p/go-libp2p-core v0.5.6
+	github.com/multiformats/go-multiaddr v0.2.2
+	github.com/multiformats/go-multiaddr-net v0.1.5
+	github.com/rs/cors v1.7.0
+)
+
+replace (
+	github.com/filecoin-project/filecoin-ffi => ../lotus/extern/filecoin-ffi
+	github.com/filecoin-project/lotus => ../lotus
+	github.com/ipfs/testground/sdk/runtime => ../../sdk/runtime
+	github.com/ipfs/testground/sdk/sync => ../../sdk/sync
+)
