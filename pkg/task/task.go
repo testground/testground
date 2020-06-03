d7e0142b74ea6f8e0bd9a4bc6792528f061f3a12
Cory Schwartz <cory@protocol.ai>
2020-06-03 11:51:39 -0700

    Include result information in task data model

--- pkg/task/task.go
+++ pkg/task/task.go
@@ -2,32 +2,69 @@ package task
 
 import "time"
 
-const (
-	TaskStateRequested TaskState = iota // when client requests a task
-	TaskStateScheduled                  // daemon accepts the task into the queue
-	TaskStateBuilding                   // daemon is building the task
-	TaskStateRunning                    // daemon is running the task
-	TaskStateComplete                   // daemon is not running the task. ready to collect.
-)
-
+// TaskState (kind: int) represents the last known state of a task.
+// A task can be in one of three states
+// TaskStateScheduled: this is the initial state of the task when it enters the queue.
+// TaskStateProcessing: once work begins on the task, it is put into this state.
+// TaskStateComplete: work is no longer being done on this task. client should check task result.
 type TaskState int
+
+const (
+	TaskStateScheduled TaskState = iota
+	TaskStateProcessing
+	TaskStateComplete
+)
 
 func (t TaskState) String() string {
 	return [...]string{
-		"requested",
-		"scheduled",
-		"building",
-		"running",
-		"complete",
+		"TaskStateRequested",
+		"TaskStateProcessing",
+		"TaskStateComplete",
 	}[t]
 }
 
-// Keep this struct simple. This structure is persited to the datastore.
+// TaskResultStatus (kind: int) is a status code for completed tasks.
+// TaskResultNone: initial status, No status, probably becasue the task is incomplete.
+// TaskResultSuccess: the task has completed without an error.
+// TaskResultFail: the task has completed with a failure.
+// TaskResultAbort: testground encountered an error and the task has not been scheduled.
+// testground will respond to client with a TaskResultAbort status if it is unable to enqueue
+// the reqeusted task.
+type TaskResultStatus int
+
+const (
+	TaskResultNone TaskResultStatus = iota
+	TaskResultSuccess
+	TaskResultFail
+	TaskResultAbort
+)
+
+func (t TaskResultStatus) String() string {
+	return [...]string{
+		"TaskResultNone",
+		"TaskResultSuccess",
+		"TaskResultFail",
+		"TaskResultAbort",
+	}[t]
+}
+
+// TaskResult (kind: struct)  contains a status code. If the status is not TaskResultNone or
+// TaskResultSuccess, relevant errors will be included in this struct.
+type TaskResult struct {
+	Status TaskResultStatus `json:"status"`
+	Errors []error          `json:"errors"`
+}
+
+// Task (kind: struct) contains metadata about a testground task. This schema is used to store
+// metadata in our task storage database as well as the wire format returned when clients get the
+// state of a running or secheduled task.
 type Task struct {
-	Priority int       `json:"priority"`
-	Created  time.Time `json:"created"`
-	ID       string    `json:"id"`
-	State    TaskState `json:"state"`
+	Version  int        `json:"version"`  // Schema version
+	Priority int        `json:"priority"` // scheduling priority
+	Created  time.Time  `json:"created"`  // datetime created
+	ID       string     `json:"id"`       // unique identifier for this task
+	State    TaskState  `json:"state"`    // State of the task
+	Result   TaskResult `json:"result"`   // result of the task, when terminal.
 }
 
 // This is a priority queue which implements container/heap.Interface
