9b3a721b13a9ce3217c757327533a7b90472fdc9
Jim Pick <jim@protocol.ai>
2020-06-15 19:22:44 +0000

    Update to latest lotus next branch

---
+++ plans/lotus-js-next-2020.06.15-8m/checkout.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# js-lotus-client
+# js-lotus-client-build
+# js-lotus-client-format
+# js-lotus-client-provider-browser
+# js-lotus-client-provider-mock
+# js-lotus-client-provider-nodejs
+# js-lotus-client-query
+# js-lotus-client-rpc
+# js-lotus-client-schema
+# js-lotus-client-testground
+
