27816f35f89395239335874325b7f5bb04920d1c
Henrique Dias <hacdias@gmail.com>
2021-06-29 09:48:53 +0200

    fix: enable Redis on k8s test

--- integration_tests/01_k8s_kind_placebo_ok.sh
+++ integration_tests/01_k8s_kind_placebo_ok.sh
@@ -14,6 +14,7 @@ docker tag $ARTIFACT testplan:placebo
 kind load docker-image testplan:placebo
 
 pushd $TEMPDIR
+testground healthcheck --runner local:docker --fix
 export REDIS_HOST=localhost
 testground run single --runner cluster:k8s --builder docker:go --use-build testplan:placebo --instances 1 --plan placebo --testcase ok --collect --wait | tee run.out
 RUNID=$(awk '/finished run with ID/ { print $9 }' run.out)
