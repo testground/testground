a70802b73178c944af37a3817e52e3c66e38e112
Laurent Senta <Laurent.Senta@gmail.com>
2022-04-20 17:00:57 +0200

    plans/example-js: fix test using new default client (#1306)

--- plans/example-js/output.js
+++ plans/example-js/output.js
@@ -1,6 +1,6 @@
 // Demonstrate test output functions
 // This method emits two Messages and one Metric (TODO)
-module.exports = async (runenv) => {
+module.exports = async (runenv, client) => {
   runenv.recordMessage('Hello, World.')
   runenv.recordMessage(`Additional arguments: ${JSON.stringify(runenv.testInstanceParams)}`)
   // runenv.R().RecordPoint("donkeypower", 3.0)
--- plans/example-js/sync.js
+++ plans/example-js/sync.js
@@ -17,65 +17,58 @@ function getRandom (min, max) {
 //
 // The leader waits until all the followers have reached the state "ready"
 // then, the followers wait for a signal from the leader to be released.
-module.exports = async (runenv) => {
+module.exports = async (runenv, client) => {
   const enrolledState = 'enrolled'
   const readyState = 'ready'
   const releasedState = 'released'
 
-  // instantiate a sync service client, binding it to the runenv.
-  const client = await sync.newBoundClient(runenv)
+  // instantiate a network client; see 'Traffic shaping' in the docs.
+  const netclient = network.newClient(client, runenv)
+  runenv.recordMessage('waiting for network initialization')
 
-  try {
-    // instantiate a network client; see 'Traffic shaping' in the docs.
-    const netclient = network.newClient(client, runenv)
-    runenv.recordMessage('waiting for network initialization')
+  // wait for the network to initialize; this should be pretty fast.
+  netclient.waitNetworkInitialized()
+  runenv.recordMessage('network initilization complete')
 
-    // wait for the network to initialize; this should be pretty fast.
-    netclient.waitNetworkInitialized()
-    runenv.recordMessage('network initilization complete')
+  // signal entry in the 'enrolled' state, and obtain a sequence number.
+  const seq = await client.signalEntry(enrolledState)
+  runenv.recordMessage(`my sequence ID: ${seq}`)
 
-    // signal entry in the 'enrolled' state, and obtain a sequence number.
-    const seq = await client.signalEntry(enrolledState)
-    runenv.recordMessage(`my sequence ID: ${seq}`)
+  // if we're the first instance to signal, we'll become the LEADER.
+  if (seq === 1) {
+    runenv.recordMessage('i am the leader.')
+    const numFollowers = runenv.testInstanceCount - 1
 
-    // if we're the first instance to signal, we'll become the LEADER.
-    if (seq === 1) {
-      runenv.recordMessage('i am the leader.')
-      const numFollowers = runenv.testInstanceCount - 1
-
-      // let's wait for the followers to signal.
-      runenv.recordMessage(`waiting for ${numFollowers} instances to become ready`)
-      const b = await client.barrier(readyState, numFollowers)
-      await b.wait
-
-      runenv.recordMessage('the followers are all ready')
-      runenv.recordMessage('ready...')
-      await sleep(1000) // 1s
-      runenv.recordMessage('set...')
-      await sleep(5000) // 5s
-      runenv.recordMessage('go, release followers!')
-
-      // signal on the 'released' state.
-      await client.signalEntry(releasedState)
-      return
-    }
-
-    const ms = getRandom(1, 5) * 1000
-    runenv.recordMessage(`i am a follower; signalling ready after ${ms} milliseconds`)
-
-    await sleep(ms)
-
-    runenv.recordMessage('follower signalling now')
-
-    // signal entry in the 'ready' state.
-    await client.signalEntry(readyState)
-
-    // wait until the leader releases us.
-    const b = await client.barrier(releasedState, 1)
+    // let's wait for the followers to signal.
+    runenv.recordMessage(`waiting for ${numFollowers} instances to become ready`)
+    const b = await client.barrier(readyState, numFollowers)
     await b.wait
 
-    runenv.recordMessage('i have been released')
-  } finally {
-    client.close()
+    runenv.recordMessage('the followers are all ready')
+    runenv.recordMessage('ready...')
+    await sleep(1000) // 1s
+    runenv.recordMessage('set...')
+    await sleep(5000) // 5s
+    runenv.recordMessage('go, release followers!')
+
+    // signal on the 'released' state.
+    await client.signalEntry(releasedState)
+    return
   }
+
+  const ms = getRandom(1, 5) * 1000
+  runenv.recordMessage(`i am a follower; signalling ready after ${ms} milliseconds`)
+
+  await sleep(ms)
+
+  runenv.recordMessage('follower signalling now')
+
+  // signal entry in the 'ready' state.
+  await client.signalEntry(readyState)
+
+  // wait until the leader releases us.
+  const b = await client.barrier(releasedState, 1)
+  await b.wait
+
+  runenv.recordMessage('i have been released')
 }
