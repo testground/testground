6cc54fde790d97fc4a41ce1bf79b873c3b4da808
Laurent Senta <laurent@singulargarden.com>
2022-06-07 14:52:42 +0200

    fix: default runner to OutcomeSuccess

--- pkg/data/result_test.go
+++ pkg/data/result_test.go
@@ -96,20 +96,15 @@ func TestDecodeTaskOutcome(t *testing.T) {
 	assert.Equal(t, task.OutcomeCanceled, r)
 	assert.Nil(t, e)
 
-	// Run with local exec runner => the result is nil, we assume outcome is unknown.
+	// Run with local exec runner => the result is nil, we assume outcome is Success if the task suceeded.
 	tested = &task.Task{
-		Type: task.TypeRun,
-		States: []task.DatedState{
-			{
-				State:   task.StateCanceled,
-				Created: time.Now(),
-			},
-		},
+		Type:   task.TypeRun,
+		States: success_state,
 		// runner outputs something like: `&api.RunOutput{RunID: input.RunID}`
 		Result: nil,
 	}
 
 	r, e = DecodeTaskOutcome(tested)
-	assert.Equal(t, task.OutcomeCanceled, r)
+	assert.Equal(t, task.OutcomeSuccess, r)
 	assert.Nil(t, e)
 }
