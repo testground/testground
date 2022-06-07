2cc5e5c0c20f871ff2e0267b834c7285e6ad1b9e
Laurent Senta <laurent@singulargarden.com>
2022-06-07 11:56:39 +0200

    feat: introduce and test data.DecodeTaskOutcome

--- pkg/data/result.go
+++ pkg/data/result.go
@@ -1,8 +1,13 @@
 // TODO: find a better name, this package is required to prevent cyclic dependencies.
 //  It's used to provide functions and tool that "connects" runners, tasks, etc.
+//  Since task is a generic wire format, and runners and builder have specific implementation,
+//   we need this module to interpret task result with run/builder specific implementation.
+// TODO: rethink the genericity model so that this module becomes unnecessary.
 package data
 
 import (
+	"fmt"
+
 	"github.com/mitchellh/mapstructure"
 	"github.com/testground/testground/pkg/logging"
 	"github.com/testground/testground/pkg/runner"
@@ -10,7 +15,9 @@ import (
 )
 
 func DecodeRunnerResult(result interface{}) *runner.Result {
-	r := &runner.Result{}
+	r := &runner.Result{
+		Outcome: task.OutcomeUnknown,
+	}
 	err := mapstructure.Decode(result, r)
 	if err != nil {
 		logging.S().Errorw("error while decoding result", "err", err)
@@ -18,6 +25,27 @@ func DecodeRunnerResult(result interface{}) *runner.Result {
 	return r
 }
 
-func DecodeTaskOutcome(t *task.Task) task.Outcome {
-	return task.OutcomeSuccess
+func DecodeTaskOutcome(t *task.Task) (task.Outcome, error) {
+	switch t.State().State {
+	case task.StateCanceled:
+		return task.OutcomeCanceled, nil
+	case task.StateProcessing:
+		return task.OutcomeUnknown, nil
+	case task.StateScheduled:
+		return task.OutcomeUnknown, nil
+	case task.StateComplete:
+		// continue
+	default:
+		return "", fmt.Errorf("unexpected task state: %s", t.State().State)
+	}
+
+	switch t.Type {
+	case task.TypeBuild:
+		// As of today a build that completed is successful. No need to check the result.
+		return task.OutcomeSuccess, nil
+	case task.TypeRun:
+		return DecodeRunnerResult(t.Result).Outcome, nil
+	default:
+		return "", fmt.Errorf("unexpected task type: %s", t.Type)
+	}
 }
