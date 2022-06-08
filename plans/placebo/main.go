aab0eb3df7a35d91fabd53b591f5f952b6449153
Laurent Senta <laurent@singulargarden.com>
2022-06-08 17:34:34 +0200

    fix: minimal init the placebo test

--- plans/placebo/main.go
+++ plans/placebo/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"time"
 
 	"github.com/testground/sdk-go/run"
 	"github.com/testground/sdk-go/runtime"
+	"github.com/testground/sdk-go/sync"
 )
 
 func main() {
@@ -13,21 +15,39 @@ func main() {
 }
 
 var testcases = map[string]interface{}{
-	"ok":    run.InitializedTestCaseFn(ok),
-	"panic": run.InitializedTestCaseFn(panickingTest),
-	"stall": run.InitializedTestCaseFn(stall),
+	"ok":    ok,
+	"panic": panickingTest,
+	"stall": stall,
 }
 
-func ok(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
+func ok(runenv *runtime.RunEnv) error {
+	minimalInit(runenv)
+
 	return nil
 }
 
-func panickingTest(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
+func panickingTest(runenv *runtime.RunEnv) error {
+	minimalInit(runenv)
+
 	panic(errors.New("this is an intentional panic"))
 }
 
-func stall(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
+func stall(runenv *runtime.RunEnv) error {
+	minimalInit(runenv)
+
 	runenv.RecordMessage("Now stalling for 24 hours")
 	time.Sleep(24 * time.Hour)
 	return nil
+}
+
+// Initialize the sync client and attach it,
+// This replaces the `run.InitializedTestCaseFn` wrapper for
+// tests that are used in integration, where the `MustWaitNetworkInitialized` call hangs.
+//
+// Do not close the client because the SDK uses this at the end of the test to send the success event
+// to the sync service.
+func minimalInit(runenv *runtime.RunEnv) {
+	ctx := context.Background()
+	client := sync.MustBoundClient(ctx, runenv)
+	runenv.AttachSyncClient(client)
 }
