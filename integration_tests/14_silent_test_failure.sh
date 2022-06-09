3ed4ffb47ffc22903a759bb5f640b7f98a91b0a7
Laurent Senta <laurent@singulargarden.com>
2022-06-09 16:25:02 +0200

    ci: allow for errors

--- integration_tests/14_silent_test_failure.sh
+++ integration_tests/14_silent_test_failure.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
-# Test for https://github.com/testground/testground/issues/TBD
-
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-source "${__dir}/header.sh"
+# Test for https://github.com/testground/testground/issues/1349
+my_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$my_dir/header.sh"
 
 testground plan import --from ./plans/_integrations --name integrations
 
@@ -12,17 +9,15 @@ pushd $TEMPDIR
 
 testground healthcheck --runner local:docker --fix
 
+# Note the `&& exit 1` lets us fail the test if the run succeeds.
+# We can't rely on $? when the run fails, because the `err_report` function in header.sh
+# runs first and succeeds (so $? is always 0).
 testground run single --plan=integrations --testcase="issue-1349-silent-failure" \
     --builder=docker:go --runner=local:docker \
-    --instances=1 --wait;
+    --instances=1 --wait && exit 1;
 
-RESULT=$?
+popd
 
 echo "terminating remaining containers"
 testground terminate --runner local:docker
 testground terminate --builder docker:go
-
-if [ RESULT = 0 ]; then
-    echo "Test succeeded, but should have failed."
-    exit 1
-fi
