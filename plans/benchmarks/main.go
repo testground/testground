a068f0b9e11674f13604725aab1f9e4a98e8207f
Darko Brdareski <dbrdareski@gmail.com>
2022-07-21 11:16:26 +0200

    Fix storm test (#1393)
    * Fix storm test
    
    * Add integration test, fix all benchmark test cases
    
    * Remove dead lines

--- plans/benchmarks/main.go
+++ plans/benchmarks/main.go
@@ -1,35 +1,18 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-
 	"github.com/testground/sdk-go/run"
-	"github.com/testground/sdk-go/runtime"
 )
 
 func main() {
-	run.Invoke(tests)
+	run.InvokeMap(testcases)
 }
 
-// Pick a different example function to run
-// depending on the name of the test case.
-func tests(runenv *runtime.RunEnv) error {
-	switch c := runenv.TestCase; c {
-	case "startup":
-		return StartTimeBench(runenv)
-	case "netinit":
-		return NetworkInitBench(runenv)
-	case "netlinkshape":
-		return NetworkLinkShapeBench(runenv)
-	case "barrier":
-		return BarrierBench(runenv)
-	case "subtree":
-		return SubtreeBench(runenv)
-	case "storm":
-		return Storm(runenv)
-	default:
-		msg := fmt.Sprintf("Unknown Testcase %s", c)
-		return errors.New(msg)
-	}
+var testcases = map[string]interface{}{
+	"startup":      run.InitializedTestCaseFn(StartTimeBench),
+	"netinit":      run.InitializedTestCaseFn(NetworkInitBench),
+	"netlinkshape": run.InitializedTestCaseFn(NetworkLinkShapeBench),
+	"barrier":      run.InitializedTestCaseFn(BarrierBench),
+	"subtree":      run.InitializedTestCaseFn(SubtreeBench),
+	"storm":        run.InitializedTestCaseFn(Storm),
 }
