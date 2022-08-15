d7e414d4b74036ac216da78f2988d9b5e27c8201
Brdji <dbrdareski@gmail.com>
2022-08-15 18:26:48 +0200

    Change pod listing code

--- plans/network/pingpong.go
+++ plans/network/pingpong.go
@@ -10,7 +10,6 @@ import (
 	"github.com/testground/sdk-go/run"
 	"github.com/testground/sdk-go/runtime"
 	"github.com/testground/sdk-go/sync"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -224,23 +223,26 @@ func getPodInfo() {
 		}
 		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
-		// Examples for error handling:
-		// - Use helper functions e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		pod, err := clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod example-xxxxx not found in default namespace\n")
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-		} else if err != nil {
-			panic(err.Error())
-		} else {
-			fmt.Printf("Found example-xxxxx pod in default namespace\n")
-			fmt.Printf("Pod name: %s  | Annotations: \n", pod.Name)
-			for k, v := range pod.GetAnnotations() {
+		for _, aPod := range pods.Items {
+			fmt.Printf("Pod name: %s  | Annotations: \n", aPod.Name)
+			for k, v := range aPod.GetAnnotations() {
 				fmt.Printf("%s->%s \n", k, v)
 			}
 		}
+
+		// Examples for error handling:
+		// - Use helper functions e.g. errors.IsNotFound()
+		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+		// pod, err := clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
+		// if errors.IsNotFound(err) {
+		// 	fmt.Printf("Pod example-xxxxx not found in default namespace\n")
+		// } else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+		// 	fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+		// } else if err != nil {
+		// 	panic(err.Error())
+		// } else {
+		// 	fmt.Printf("Found example-xxxxx pod in default namespace\n")
+		// }
 
 		time.Sleep(10 * time.Second)
 	}
