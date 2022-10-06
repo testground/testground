b15183f93a21e54f2fb51cb87a8ba4f7316ee3d7
ackintosh <sora.akatsuki@gmail.com>
2022-10-07 08:11:05 +0900

    Fix missing env var INFLUXDB_URL
    The sidecar shows the warning message on console:
    MESSAGE << sidecar >> InfluxDB unavailable; no metrics will be dispatched: no InfluxDB URL in $INFLUXDB_URL env var

--- pkg/runner/local_docker.go
+++ pkg/runner/local_docker.go
@@ -150,7 +150,7 @@ func (r *LocalDockerRunner) Healthcheck(ctx context.Context, engine api.Engine, 
 			Entrypoint: []string{"testground"},
 			Cmd:        []string{"sidecar", "--runner", "docker"},
 			// NOTE: we export REDIS_HOST for compatibility with older sdk versions.
-			Env: []string{"SYNC_SERVICE_HOST=testground-sync-service", "REDIS_HOST=testground-redis", "INFLUXDB_HOST=testground-influxdb", "GODEBUG=gctrace=1", additionalHosts},
+			Env: []string{"SYNC_SERVICE_HOST=testground-sync-service", "REDIS_HOST=testground-redis", "INFLUXDB_HOST=testground-influxdb", "INFLUXDB_URL=http://testground-influxdb:8086", "GODEBUG=gctrace=1", additionalHosts},
 		},
 		HostConfig: &container.HostConfig{
 			PublishAllPorts: true,
@@ -739,7 +739,7 @@ func attachContainerToNetwork(ctx context.Context, cli *client.Client, container
 	return cli.NetworkConnect(ctx, networkID, containerID, nil)
 }
 
-//nolint this function is unused, but it may come in handy.
+// nolint this function is unused, but it may come in handy.
 func detachContainerFromNetwork(ctx context.Context, cli *client.Client, containerID string, networkID string) error {
 	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
