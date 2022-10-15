a178451247cde07909e641e05af6ced759aacc35
glendc <glen.decauwsemaecker@otainsight.com>
2022-10-15 15:55:18 +0200

    enable all tests for node (non-browser) example

--- integration_tests/example_05_browser_node.sh
+++ integration_tests/example_05_browser_node.sh
@@ -16,9 +16,53 @@ docker tag $ARTIFACT testplan:example-browser-node
 
 testground healthcheck --runner local:docker --fix
 
+# Node: success
+
+testground run single \
+    --plan=testground/example-browser-node \
+    --testcase=output \
+    --builder=docker:node \
+    --use-build=testplan:example-browser-node \
+    --runner=local:docker \
+    --instances=1 \
+    --collect \
+    --wait | tee run.out
+
+assert_run_outcome_is run.out "success"
+
+# Node: success
+
+testground run single \
+    --plan=testground/example-browser-node \
+    --testcase=failure \
+    --builder=docker:node \
+    --use-build=testplan:example-browser-node \
+    --runner=local:docker \
+    --instances=1 \
+    --collect \
+    --wait | tee run.out
+
+assert_run_outcome_is run.out "failure"
+
+# Node: ping pong
+
 testground run single \
     --plan=testground/example-browser-node \
     --testcase=pingpong \
+    --builder=docker:node \
+    --use-build=testplan:example-browser-node \
+    --runner=local:docker \
+    --instances=2 \
+    --collect \
+    --wait | tee run.out
+
+assert_run_output_is_correct run.out
+
+# Node: sync
+
+testground run single \
+    --plan=testground/example-browser-node \
+    --testcase=sync \
     --builder=docker:node \
     --use-build=testplan:example-browser-node \
     --runner=local:docker \
