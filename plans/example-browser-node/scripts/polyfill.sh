e97aece5f4c770f45d3db0d5f4ea687b41340b18
glendc <glen.decauwsemaecker@otainsight.com>
2022-11-08 01:57:30 +0100

    remove shimming of @testground/sdk from this code

--- plans/example-browser-node/scripts/polyfill.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# polyfill setimmediate for winston in browser
-sed "1s/^/require('setimmediate')\n\n/" src/index.js > src/index_polyfill.js
-mv src/index_polyfill.js src/index.js
--- plans/example-browser-node/webpack.config.js
+++ plans/example-browser-node/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const webpack = require('webpack')
 const source = path.resolve(__dirname, 'src')
 
 module.exports = {
@@ -19,29 +18,5 @@ module.exports = {
       use: 'babel-loader'
     }]
   },
-  resolve: {
-    extensions: ['.js'],
-    fallback: {
-      fs: false,
-      os: require.resolve('os-browserify'),
-      path: require.resolve('path-browserify'),
-      https: require.resolve('https-browserify'),
-      crypto: require.resolve('crypto-browserify'),
-      http: require.resolve('stream-http'),
-      zlib: require.resolve('browserify-zlib'),
-      buffer: require.resolve('buffer/'),
-      url: require.resolve('url/'),
-      'assert/': require.resolve('assert/'),
-      stream: require.resolve('stream-browserify')
-    }
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer']
-    }),
-    new webpack.ProvidePlugin({
-      process: 'process/browser'
-    })
-  ],
   mode: 'development'
 }
