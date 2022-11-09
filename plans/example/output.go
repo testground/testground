a4ccc7568fc4066b7d5236183264a7f857f79517
glendc <glen.decauwsemaecker@otainsight.com>
2022-11-10 00:23:33 +0100

    add environment_variables to Example plan

--- plans/example/output.go
+++ plans/example/output.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/testground/sdk-go/run"
 	"github.com/testground/sdk-go/runtime"
 )
@@ -8,7 +10,11 @@ import (
 // Demonstrate test output functions
 // This method emits two Messages and one Metric
 func ExampleOutput(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
-	runenv.RecordMessage("Hello, World.")
+	if msg := os.Getenv("TESTGROUND_HELLO_MESSAGE"); msg != "" {
+		runenv.RecordMessage(msg)
+	} else {
+		runenv.RecordMessage("Hello, World.")
+	}
 	runenv.RecordMessage("Additional arguments: %d", len(runenv.TestInstanceParams))
 	runenv.R().RecordPoint("donkeypower", 3.0)
 	return nil
