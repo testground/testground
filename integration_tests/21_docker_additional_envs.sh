792178b7b230488b8d650d4799acedc2564063b5
glendc <glen.decauwsemaecker@otainsight.com>
2022-11-12 09:50:00 +0100

    finish GO test
    dropped the bash-generic test as it would be too much effort
    to create also the out files etc :)

--- integration_tests/21_docker_additional_envs.sh
+++ integration_tests/21_docker_additional_envs.sh
@@ -8,31 +8,6 @@ testground plan import --from ./plans --name testground
 
 pushd $TEMPDIR
 
-# Generic Container
-
-testground build single \
-    --plan testground/additional_envs \
-    --builder docker:generic \
-    --wait | tee build.out
-export ARTIFACT=$(awk -F\" '/generated build artifact/ {print $8}' build.out)
-docker tag $ARTIFACT testplan:additional_envs
-
-
-testground run single \
-    --plan=testground/additional_envs \
-    --testcase=additional_envs \
-    --builder=docker:generic \
-    --use-build=testplan:additional_envs \
-    --runner=local:docker \
-    --instances=1 \
-    --collect \
-    --wait | tee run.out
-
-assert_run_output_is_correct run.out
-assert_run_outcome_is ./run.out "success"
-
-# Go Docker
-
 testground build single \
     --plan testground/additional_envs \
     --builder docker:go \
--- plans/additional_envs/main.go
+++ plans/additional_envs/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 
 	"github.com/testground/sdk-go/run"
 	"github.com/testground/sdk-go/runtime"
 )
+
+const ENV_KEY = "MY_FIRST_CUSTOM_ENV_VARIABLE"
 
 func main() {
 	run.InvokeMap(map[string]interface{}{
@@ -16,12 +17,12 @@ func main() {
 }
 
 func additionalEnvs(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
-	msg, found := os.LookupEnv("TESTGROUND_HELLO_MESSAGE")
+	msg, found := os.LookupEnv(ENV_KEY)
 	if !found {
-		return errors.New("No env set with key: TESTGROUND_HELLO_MESSAGE")
+		return fmt.Errorf("No env set with key: %s", ENV_KEY)
 	}
 	if msg != "Hello, AdditionalEnvs!" {
-		return fmt.Errorf("unexpected TESTGROUND_HELLO_MESSAGE: %q", msg)
+		return fmt.Errorf("unexpected %s: %q", ENV_KEY, msg)
 	}
 	return nil
 }
