446e886325bc493a50c783c5f2700add180491ae
Laurent Senta <laurent@singulargarden.com>
2022-11-17 11:14:13 +0100

    example: demo structured output

--- plans/example/output.go
+++ plans/example/output.go
@@ -11,5 +11,17 @@ func ExampleOutput(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
 	runenv.RecordMessage("Hello, World.")
 	runenv.RecordMessage("Additional arguments: %d", len(runenv.TestInstanceParams))
 	runenv.R().RecordPoint("donkeypower", 3.0)
+
+	// Demo structured assets:
+	// After you `testground run` this, you may use `testground collect <runid>` to
+	// get a tar which contains the test outputs for every instances, including a `demo.out` file.
+	_, l, err := runenv.CreateStructuredAsset("demo.out", runtime.StandardJSONConfig())
+
+	if (err != nil) {
+		return err
+	}
+
+	l.Info("This is a structured output message")
+
 	return nil
 }
