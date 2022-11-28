8629aa281a02bf6a193573a8e0e3959307aadf1d
Darko Brdareski <dbrdareski@gmail.com>
2022-11-28 11:03:10 +0100

    eks: changes required to support EKS cluster (#1350)
    This review contains all the required changes for the new EKS cluster. Most of the changes relate to network annotations, IP ranges, configuration options, etc.
    
    Closes #1499
    A related change in the infra: testground/infra#78
    
    Co-authored-by: AbominableSnowman730 <abominablesnowman730@gmail.com>
    Co-authored-by: LudiSistemas <portalscg@gmail.com>
    Co-authored-by: Laurent Senta <laurent@singulargarden.com>

--- pkg/engine/supervisor.go
+++ pkg/engine/supervisor.go
@@ -46,7 +46,6 @@ func (e *Engine) deleteSignal(id string) {
 
 func (e *Engine) worker(n int) {
 	logging.S().Infow("supervisor worker started", "worker_id", n)
-
 	taskTimeout := 10 * time.Minute
 	if e.EnvConfig().Daemon.Scheduler.TaskTimeoutMin != 0 {
 		taskTimeout = time.Duration(e.EnvConfig().Daemon.Scheduler.TaskTimeoutMin) * time.Minute
