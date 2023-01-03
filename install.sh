ab207216fdaddd31f48e94ce577ca7a5723c4686
galargh <piotr.galar@gmail.com>
2023-01-03 14:29:55 +0100

    chore: make install.sh install testground to pwd

--- install.sh
+++ install.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env sh
 set -e
 set -o pipefail
-unameOut="$(uname -s)"
-TESTGROUND_HOME="$HOME/.config/testground"
-case "${unameOut}" in
-    Darwin*)    TESTGROUND_HOME="$HOME/Library/Application Support";;
-esac
-
-mkdir -p "$TESTGROUND_HOME" && cd "$TESTGROUND_HOME"
 
 docker pull iptestground/testground:edge
 docker pull iptestground/sync-service:edge
 docker pull iptestground/sidecar:edge
 
 # At the moment this is the fastest way to get a pre-built testground binary.
-docker run -v ${PWD}:/mount --rm --entrypoint cp iptestground/testground:edge /testground /mount/testground
+id="$(docker create iptestground/testground:edge)"
+docker cp $id:/testground $PWD/testground
+docker rm $id
 
 if [ -z $GITHUB_PATH ]; then
-    echo "Testground was installed to \"$TESTGROUND_HOME\". Add this to your path."
+    echo "Testground was installed to \"$PWD\". Add this to your path."
 else
-    echo "$TESTGROUND_HOME" >> $GITHUB_PATH
+    echo "$PWD" >> $GITHUB_PATH
 fi
